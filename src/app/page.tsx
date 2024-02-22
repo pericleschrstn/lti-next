578842739bcb0dc732a489c7510d6a9e2dcdbca1
Pericles Christian <pericleschrstn@gmail.com>
2024-02-22 15:29:06 -0300

    feat: component accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { Button } from "@/components/ui/button";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 export default function Home() {
   return (
     <div className="p-5">
       <h1 className="text-3xl text-primary font-bold">Hello World</h1>
-      <Button>Clica aqui</Button>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
