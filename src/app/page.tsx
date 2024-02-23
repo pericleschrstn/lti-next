e5484a0da9b0e5f80ce4dd348e9b9d012f84815e
Pericles Christian <pericleschrstn@gmail.com>
2024-02-23 14:43:06 -0300

    design system: checkbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
+import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 export default function Home() {
   return (
     <div className="p-5">
-      <h1 className="text-3xl text-primary font-bold">Hello World</h1>
-      <RadioGroup defaultValue="option-one">
-        <div className="flex space-x-2 items-center ">
-          <RadioGroupItem value="option-one" id="option-one" />
-          <Label htmlFor="option-one">Item 1</Label>
-        </div>
-        <div className="flex space-x-2 items-center ">
-          <RadioGroupItem value="option-two" id="option-two" />
-          <Label htmlFor="option-two">Item 2</Label>
-        </div>
-      </RadioGroup>
+      <h1 className="text-3xl text-primary font-bold mb-3">Hello World</h1>
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <Label htmlFor="terms">Aceite os termos</Label>
+      </div>
     </div>
   );
 }
