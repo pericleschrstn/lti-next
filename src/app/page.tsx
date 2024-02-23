44815fda691f6674390d4f3c52c984413bb93894
Pericles Christian <pericleschrstn@gmail.com>
2024-02-23 17:03:55 -0300

    design system: input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
     <div className="p-5">
       <h1 className="text-3xl text-primary font-bold mb-3">Hello World</h1>
-      <div className="flex items-center space-x-2">
-        <Checkbox id="terms" />
-        <Label htmlFor="terms">Aceite os termos</Label>
+      <div>
+        <Label htmlFor="name">Seu nome</Label>
+        <Input id="name" type="text" placeholder="Seu nome" />
       </div>
     </div>
   );
