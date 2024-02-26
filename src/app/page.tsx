cf6ec6b4552a5489d548385d01d46f59b6074149
Pericles Christian <pericleschrstn@gmail.com>
2024-02-26 11:10:40 -0300

    design system: input and textarea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
     <div className="p-5">
       <h1 className="text-3xl text-primary font-bold mb-3">Hello World</h1>
       <div>
-        <Label htmlFor="name">Seu nome</Label>
-        <Input id="name" type="text" placeholder="Seu nome" />
+        <Label htmlFor="name">Mensagem</Label>
+        <Textarea id="name" className="mb-4" placeholder="Input" />
+        <Input placeholder="Input" />
       </div>
     </div>
   );
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
     <input
       type={type}
       className={cn(
-        "flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus:outline-none focus:border-primary focus:ring focus-visible:outline-none focus-visible:ring focus-visible:ring-gray-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-sky-900 dark:focus:ring-sky-900 dark:focus:outline-none",
+        "flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm text-body ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus:outline-none focus:border-primary focus:ring focus-visible:outline-none focus-visible:ring disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-sky-900 dark:focus:ring-sky-900 dark:focus:outline-none dark:text-gray-200",
         className
       )}
       ref={ref}
