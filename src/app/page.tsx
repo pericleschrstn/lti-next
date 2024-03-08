e23f2b687f0b0e3afec20f3c94e243b92846e9ca
Pericles Christian <pericleschrstn@gmail.com>
2024-03-08 17:58:25 -0300

    feat: add component sheet and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,6 @@
-import List from "@/components/list";
+import Header from "@/components/header";
+import logotipo from "@/assets/logotipo-fiemg-white.svg";
 
 export default function Home() {
-  const listItens = [
-    {
-      title: "Olá, mundo",
-      content: "Isso é um texto...",
-    },
-    {
-      title: "Titulo 2",
-      content: "Outro texto...",
-    },
-    {
-      title: "Titulo 3",
-      content: "Mais um texto...",
-    },
-  ];
-
-  return (
-    <div className="p-5">
-      <h1 className="text-3xl text-primary font-bold mb-3">Hello World</h1>
-      <List items={listItens} />
-    </div>
-  );
+  return <Header logotipoSrc={logotipo} />;
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,48 @@
+import { VariantProps, cva } from "class-variance-authority";
+import Image from "next/image";
+import React from "react";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+const headerVariants = cva("p-3", {
+  variants: {
+    intent: {
+      primary: "bg-primary",
+      secondary: "bg-secondary",
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+  },
+});
+
+interface HeaderProps extends VariantProps<typeof headerVariants> {
+  logotipoSrc: string;
+}
+
+export default function Header({ intent, logotipoSrc, ...props }: HeaderProps) {
+  return (
+    <header className={headerVariants({ intent })} {...props}>
+      <div className="mx-auto max-w-7xl">
+        <nav className="flex justify-between">
+          <Image src={logotipoSrc} alt="Logotipo"></Image>
+          <Sheet>
+            <SheetTrigger>
+              <Menu />
+              Menu
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete your account and remove your data from our
+                  servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </nav>
+      </div>
+    </header>
+  );
+}
